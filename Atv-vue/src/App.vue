536b34ed1cf6fd8d90227e644bc2a61f1408e772
Dudda-soares <eduarda.soaresdsantos@gmail.com>
2025-07-18 17:18:22 -0300

    atividade

--- Atv-vue/src/App.vue
+++ Atv-vue/src/App.vue
@@ -1,47 +1,51 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { computed, ref } from 'vue';
+
+const numero1= ref(0)
+const numero2= ref(0)
+const operacao = ref('somar')
+
+const total = computed(()=>{
+
+    switch(operacao.value){
+        case 'somar':
+            return numero1.value + numero2.value;
+        case 'subtrair' :
+            return numero1.value - numero2.value;
+        case 'multiplicar':
+            return numero1.value * numero2.value;
+        case 'dividir':
+            return numero2.value !==0 ? numero1.value / numero2.value : 'erro na divisão'
+        default:
+            return 0
+    }
+    
+})
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="container">
+        <h1 class="p-5 ">Calculadora Aritmetica</h1>
+        <div class="calculadora" >
+            <label class="m-2">Insira o primeiro Valor:  </label>
+            <input  type="number"  v-model="numero1"/>
+            <label class="m-2" >Insira o segundo Valor: </label>
+            <input  type="number" name="" id="" v-model="numero2"/>
+            <label class="m-2" >Escolha a Operação: </label> 
+            <select v-model="operacao" class="m-2">
+              <option value="somar">Somar</option>
+              <option  value="subtrair">Subtrair</option>
+              <option value="multiplicar">Multiplicar</option>
+              <option value="dividir">dividir</option>
+            </select>
+          
+            <p class="m-5 ">Total: {{ total }}</p>
+          </div>
+       
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
